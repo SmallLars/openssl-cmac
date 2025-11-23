198a310f23b4251c86ff2815489ba3fa68748f18
Lars Schmertmann <SmallLars@t-online.de>
2025-11-23 12:06:49 +0100

    Use [] instead of () for %w
    Rubocop:
      C: Style/PercentLiteralDelimiters:
         %w-literals should be delimited by [ and ].

--- test/test_cmac.rb
+++ test/test_cmac.rb
@@ -16,19 +16,19 @@ class CMACTest < Test::Unit::TestCase
            'ae2d8a571e03ac9c9eb76fac45af8e51'\
            '30c81c46a35ce411e5fbc1191a0a52ef'\
            'f69f2445df4f9b17ad2b417be66c3710'].pack('H*')]
-  MAC = %w(bb1d6929e95937287fa37d129b756746
+  MAC = %w[bb1d6929e95937287fa37d129b756746
            070a16b46b4d4144f79bdd9dd04a287c
            dfa66747de9ae63030ca32611497c827
-           51f0bebf7e3b9d92fc49741779363cfe)
+           51f0bebf7e3b9d92fc49741779363cfe]
 
   # http://tools.ietf.org/html/rfc4615#section-4
   PRF_KEYS = [['000102030405060708090a0b0c0d0e0fedcb'].pack('H*'),
               ['000102030405060708090a0b0c0d0e0f'].pack('H*'),
               ['00010203040506070809'].pack('H*')]
   PRF_DATA = ['000102030405060708090a0b0c0d0e0f10111213'].pack('H*')
-  PRF_OUTS = %w(84a348a4a45d235babfffc0d2b4da09a
+  PRF_OUTS = %w[84a348a4a45d235babfffc0d2b4da09a
                 980ae87b5f4c9c5214f5b6a8455e4c2d
-                290d9e112edb09ee141fcf64c0b72f3d)
+                290d9e112edb09ee141fcf64c0b72f3d]
 
   def test_init_failure
     assert_raise(OpenSSL::CMACError) { OpenSSL::CMAC.new('DES') }
